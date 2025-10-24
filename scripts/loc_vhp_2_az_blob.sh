1897fc64a9b594121fa0b957625204ca6182f885
Arthur Cho <hello@memoirji.com>
2025-10-24 08:38:23 +0000

    fix wrong py file name

--- scripts/loc_vhp_2_az_blob.sh
+++ scripts/loc_vhp_2_az_blob.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -euo pipefail
-uv run python scripts/loc_vhp_2_blob.py \
+uv run python scripts/loc_vhp_2_az_blob.py \
   --parquet data/raw/loc/veterans_history_project_resources.parquet \
   --prefix loc_vhp
